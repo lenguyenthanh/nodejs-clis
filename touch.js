8766373c87b05b2465b9253116ae54c8bd008071
Thanh Le <lenguyenthanh@hotmail.com>
2016-10-20 22:13:29 +0700

    cil-touch can touch multiple files now

--- touch.js
+++ touch.js
@@ -2,11 +2,16 @@
 
 const helper = require('./helper')
 const fs = require('fs').promise
+const _ = require('lodash')
 
 async function touch() {
-  const filePath = process.argv[2] || ''
-  const fd = await fs.open(filePath, 'a+')
-  const fstats = await fs.stat(filePath)
+  const files = _.drop(process.argv, 2)
+  files.forEach(doTouch)
+}
+
+async function doTouch(file) {
+  const fd = await fs.open(file, 'a+')
+  const fstats = await fs.stat(file)
 
   await fs.futimes(fd, fstats.atime, new Date())
   await fs.close(fd)
